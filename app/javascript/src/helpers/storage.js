b53adce26ef62ec8dd6a83c6a128a9e676800454
Karan Sapolia <sapoliakaran@gmail.com>
2021-07-12 17:08:18 +0530

    Add userName to localStorage

--- app/javascript/src/helpers/storage.js
+++ app/javascript/src/helpers/storage.js
@@ -1,7 +1,8 @@
-const setToLocalStorage = ({ authToken, email, userId }) => {
+const setToLocalStorage = ({ authToken, email, userId, userName }) => {
   localStorage.setItem("authToken", authToken);
   localStorage.setItem("authEmail", email);
   localStorage.setItem("authUserId", userId);
+  localStorage.setItem("authUserName", userName);
 };
 
 const getFromLocalStorage = key => {
